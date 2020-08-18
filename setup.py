c621826a38697c9f3e3fa3ff40455de7f7102232
Rodrigo Lima <rodrigolima82@gmail.com>
2020-08-17 23:34:53 -0300

    Upload My Custom SKlearn

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='my_custom_sklearn_transforms',
+    version='1.0',
+    description='''
+            Custom transforms from scikit-learn into Watson Machine Learning
+                ''',
+    url='https://github.com/rodrigolima82/sklearn_transforms',
+    author='Rodrigo Lima Oliveira',
+    author_email='rodrigolima82@gmail.com',
+    license='BSD',
+    packages=[
+        'my_custom_sklearn_transforms'
+    ],
+    zip_safe=False,
+    install_requires=[
+        'imbalanced-learn==0.4.3',
+    ],
+)
