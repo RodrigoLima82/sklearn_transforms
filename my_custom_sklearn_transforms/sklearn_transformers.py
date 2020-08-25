5a3ff4bc6d0808edbdaf49557638e14fb20e565d
Rodrigo de Lima Oliveira <rodrigolima82@gmail.com>
2020-08-25 10:23:39 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -25,9 +25,29 @@ class NewColumns(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
     
-    def transform(self, X):
-        X[self.column+'_1'] = np.sqrt(X[[self.column]].sum(axis=1))
-        X[self.column+'_2'] = np.tan(np.sqrt(X[[self.column]].sum(axis=1)))
+    def transform(self, X):              
+        #X[self.column+'_1'] = np.sqrt(X[[self.column]].sum(axis=1))
+        #X[self.column+'_2'] = np.tan(np.sqrt(X[[self.column]].sum(axis=1)))
+        
+        # Defini as colunas que serao agregadas
+        cols   =  [['NOTA_DE'],['NOTA_EM'],['NOTA_MF'],['NOTA_GO']]
+        target = 'PERFIL'
+
+        # Percorre as colunas da lista para agregacao
+        for col in cols:
+            col_name = '_'.join(col)+'_'
+            X[col_name + 'mean'] = X.groupby(col)[target].transform('mean').astype(np.float16)
+            X[col_name + 'median'] = X.groupby(col)[target].transform('median').astype(np.float16)
+            X[col_name + 'max'] = X.groupby(col)[target].transform('max').astype(np.float16)
+            X[col_name + 'min'] = X.groupby(col)[target].transform('min').astype(np.float16)
+            X[col_name + 'std'] = X.groupby(col)[target].transform('std').astype(np.float16)
+            X[col_name + 'range'] = X[col_name + 'max'] - X[col_name + 'min']
+            X[col_name + 'skew'] = X.groupby(col)[target].transform('skew').astype(np.float16)
+            X[col_name + 'mad'] = X.groupby(col)[target].transform('mad').astype(np.float16)
+            X[col_name + 'q25'] = X.groupby(col)[target].transform(lambda x: x.drop_duplicates().quantile(0.25)).astype(np.float16)
+            X[col_name + 'q75'] = X.groupby(col)[target].transform(lambda x: x.drop_duplicates().quantile(0.75)).astype(np.float16)
+            X[col_name + 'q95'] = X.groupby(col)[target].transform(lambda x: x.drop_duplicates().quantile(0.95)).astype(np.float16)
+            X[col_name + 'iqr'] = X[col_name + 'q75'] - dados[col_name + 'q25']                
 
         return X
     
