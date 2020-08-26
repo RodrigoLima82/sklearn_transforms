cf1f1ee99510bd2e604cfb414d60ccc7ba13191b
Rodrigo de Lima Oliveira <rodrigolima82@gmail.com>
2020-08-26 12:31:50 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder
 
 
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -12,44 +13,9 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
     
-class NewColumns(BaseEstimator, TransformerMixin):
-    
-    def __init__(self, column):
-        self.column = column
-    
-    def fit(self, X, y=None):
-        return self
-    
-    def transform(self, X):              
-        #X[self.column+'_1'] = np.sqrt(X[[self.column]].sum(axis=1))
-        #X[self.column+'_2'] = np.tan(np.sqrt(X[[self.column]].sum(axis=1)))
-        
-        # Defini as colunas que serao agregadas
-        cols   =  [['NOTA_DE'],['NOTA_EM'],['NOTA_MF'],['NOTA_GO']]
-        target = 'PERFIL'
-
-        # Percorre as colunas da lista para agregacao
-        for col in cols:
-            col_name = '_'.join(col)+'_'
-            X[col_name + 'mean'] = X.groupby(col)[target].transform('mean').astype(np.float16)
-            X[col_name + 'median'] = X.groupby(col)[target].transform('median').astype(np.float16)
-            X[col_name + 'max'] = X.groupby(col)[target].transform('max').astype(np.float16)
-            X[col_name + 'min'] = X.groupby(col)[target].transform('min').astype(np.float16)
-            X[col_name + 'std'] = X.groupby(col)[target].transform('std').astype(np.float16)
-            X[col_name + 'range'] = X[col_name + 'max'] - X[col_name + 'min']
-            X[col_name + 'skew'] = X.groupby(col)[target].transform('skew').astype(np.float16)
-            X[col_name + 'mad'] = X.groupby(col)[target].transform('mad').astype(np.float16)
-            X[col_name + 'q25'] = X.groupby(col)[target].transform(lambda x: x.drop_duplicates().quantile(0.25)).astype(np.float16)
-            X[col_name + 'q75'] = X.groupby(col)[target].transform(lambda x: x.drop_duplicates().quantile(0.75)).astype(np.float16)
-            X[col_name + 'q95'] = X.groupby(col)[target].transform(lambda x: x.drop_duplicates().quantile(0.95)).astype(np.float16)
-            X[col_name + 'iqr'] = X[col_name + 'q75'] - dados[col_name + 'q25']                
-
-        return X
     
 class SetIndex(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -59,9 +25,7 @@ class SetIndex(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
         return data.set_index(self.columns, inplace=True)
     
     
@@ -74,10 +38,31 @@ class MapEncode(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         data = X.copy()
-        data[self.column]  = data[self.column].map({'Médio completo': 0, 
+        data['Educacao']  = data['Educacao'].map({'Médio completo': 0, 
                                                     'Superior incompleto': 1,
                                                     'Superior incompleto - cursando': 2,
                                                     'Superior completo': 3,
                                                     'Pós-gradução': 4})
         
         return data
+    
+class CatEncode(BaseEstimator, TransformerMixin):
+    def __init__(self, features):
+        self.features = features
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+
+        non_features = self.features
+
+        le = LabelEncoder()
+
+        for features in non_features:
+            fe_labels = le.fit_transform(data[features])
+            data[features] = fe_labels
+            fe_mappings = {index: label for index, label in enumerate(le.classes_)}
+
+        return data    
